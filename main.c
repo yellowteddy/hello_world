a86319964ae6abae4533a088f58eeaaa6a1c6f43
yellowteddy <yellowteddybeer@gmail.com>
2020-07-12 12:36:31 +0600

    added the add function

--- main.c
+++ main.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 
+int add(int a, int b)
+{
+    return a+b;
+}
+
 int main()
 {
+    int a = 10; 
+    int b = 20;
     printf("this is a simple calculator program\n");
+    printf("addition of %d + %d = %d\n",a,b,add(a,b));
 
     return 0;
 }
